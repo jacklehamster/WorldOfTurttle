819d4e53cb612986f98ca29a258ad809ac5d50a9
Vincent Le Quang <vlequang@adobe.com>
2014-04-12 22:20:38 -0700

    Initial Commit

---
+++ WorldOfTurttle/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  WorldOfTurttle
+//
+//  Created by Vincent Le Quang on 4/12/14.
+//  Copyright (c) 2014 Dobuki Studio. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
