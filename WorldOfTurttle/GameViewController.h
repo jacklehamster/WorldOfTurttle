7a4ab059aef0ebd4540ac8d2a42ee399338f5fc9
Vincent Le Quang <vlequang@adobe.com>
2014-04-14 11:59:08 -0700

    World of Turtle for IOS - work in progress

---
+++ WorldOfTurttle/GameViewController.h
@@ -0,0 +1,14 @@
+//
+//  GameViewController.h
+//  WorldOfTurttle
+//
+//  Created by Vincent Le Quang on 4/12/14.
+//  Copyright (c) 2014 Dobuki Studio. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <SpriteKit/SpriteKit.h>
+
+@interface GameViewController : UIViewController
+
+@end
---
+++ WorldOfTurttle/GameViewController.m
@@ -0,0 +1,67 @@
+//
+//  GameViewController.m
+//  WorldOfTurttle
+//
+//  Created by Vincent Le Quang on 4/12/14.
+//  Copyright (c) 2014 Dobuki Studio. All rights reserved.
+//
+
+#import "GameViewController.h"
+#import "MyScene.h"
+
+@interface GameViewController ()
+
+@end
+
+@implementation GameViewController
+
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    
+    // Configure the view.
+    SKView * skView = (SKView *)self.view;
+    skView.showsFPS = YES;
+    skView.showsNodeCount = YES;
+    
+    // Create and configure the scene.
+    SKScene * scene = [MyScene sceneWithSize:skView.bounds.size];
+    scene.scaleMode = SKSceneScaleModeAspectFill;
+    
+    // Present the scene.
+    [skView presentScene:scene];
+}
+
+- (BOOL)shouldAutorotate
+{
+    return YES;
+}
+- (NSUInteger)supportedInterfaceOrientations
+{
+    if ([[UIDevice currentDevice] userInterfaceIdiom] == UIUserInterfaceIdiomPhone) {
+        return UIInterfaceOrientationMaskAllButUpsideDown;
+    } else {
+        return UIInterfaceOrientationMaskAll;
+    }
+}
+
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender
+{
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
